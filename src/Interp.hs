18d2455a86da48fc586265f99b1b6315604db924
gonzalob <gonzalob@zedplan.com>
2023-03-31 19:56:49 -0300

    execute Ejemplo

--- src/Interp.hs
+++ src/Interp.hs
@@ -6,22 +6,20 @@ module Interp (
 ) where
 
 import Graphics.Gloss(Picture, Display(InWindow), makeColorI, color, pictures, translate, white, display)
-import Dibujo (Dibujo, foldDib)
+import Dibujo (Dibujo, foldDib, rotar, espejar, rot45, apilar, juntar, encimar)
 import FloatingPic (FloatingPic, Output, grid)
-
 
 -- Interpretación de un dibujo
 -- formulas sacadas del enunciado
--- a -> Vector -> Vector -> Vector -> Picture -> (Dibujo a) -> Vector -> Vector -> Vector -> Picture
 interp :: Output a -> Output (Dibujo a)
-interp = undefined
--- interp (Figura f) fig = figura fig
--- interp (Rotar d) fig = rotar (interp d fig)
--- interp (Espejar d) fig = reflejaX (interp d fig)
--- interp (Rot45 d) fig = rotate (pi / 4) (interp d fig)
--- interp (Apilar x y d1 d2) fig = apilar x y (interp d1 fig) (interp d2 fig)
--- interp (Juntar x y d1 d2) fig = juntar x y (interp d1 fig) (interp d2 fig)
--- interp (Encimar d1 d2) fig = encimar (interp d1 fig) (interp d2 fig)
+interp fun fig = fun (foldDib fFigura fRotar fEspejar fRot45 fApilar fJuntar fEncimar fig)
+    where   fFigura = id
+            fRotar = id
+            fEspejar = id
+            fRot45 = id
+            fApilar x y d1 d2 = d1
+            fJuntar x y d1 d2 = d1
+            fEncimar d1 d2 = d1
 
 -- Configuración de la interpretación
 data Conf = Conf {
