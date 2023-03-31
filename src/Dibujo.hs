84c5b912a56cde2ea63c1924434cbc1a43a0ff67
gonzalob <gonzalob@zedplan.com>
2023-03-31 09:33:51 -0300

    first idea of Dibujo

--- src/Dibujo.hs
+++ src/Dibujo.hs
@@ -1,32 +1,61 @@
 {-# LANGUAGE LambdaCase #-}
-module Dibujo (
-    Dibujo,
-    figura, rotar, espejar, rot45, apilar, juntar, encimar,
-    r180, r270,
-    (.-.), (///), (^^^),
-    cuarteto, encimar4, ciclar,
-    foldDib, mapDib,
-    figuras
-) where
 
+module Dibujo
+  ( Dibujo,
+    figura,
+    rotar,
+    espejar,
+    rot45,
+    apilar,
+    juntar,
+    encimar,
+    r180,
+    r270,
+    (.-.),
+    (///),
+    (^^^),
+    cuarteto,
+    encimar4,
+    ciclar,
+    foldDib,
+    mapDib,
+    figuras,
+  )
+where
 
 {-
 Gramática de las figuras:
 <Fig> ::= Figura <Bas> | Rotar <Fig> | Espejar <Fig> | Rot45 <Fig>
-    | Apilar <Float> <Float> <Fig> <Fig> 
-    | Juntar <Float> <Float> <Fig> <Fig> 
+    | Apilar <Float> <Float> <Fig> <Fig>
+    | Juntar <Float> <Float> <Fig> <Fig>
     | Encimar <Fig> <Fig>
 -}
 
-
-data Dibujo a = Borrar -- Completar
-    deriving (Eq, Show)
+data Dibujo a
+  = Figura a
+  | Rotar (Dibujo a)
+  | Espejar (Dibujo a)
+  | Rot45 (Dibujo a)
+  | Apilar Float Float (Dibujo a) (Dibujo a)
+  | Juntar Float Float (Dibujo a) (Dibujo a)
+  | Encimar (Dibujo a) (Dibujo a)
+  deriving (Eq, Show)
 
 -- Agreguen los tipos y definan estas funciones
+-- type Punto = (Float, Float)
+-- type Basica = Punto -> Punto -> Punto
+
+-- type Figura =  Rectangulo | Triangulo
+
+-- triangulo :: Basica -> Picture
+-- triangulo a b c = polygon [a, b, c]
+
+-- rectangulo :: Basica -> Picture
+-- rectangulo a b c = polygon [a, b, a + c, b + c]
 
 -- Construcción de dibujo. Abstraen los constructores.
-
-figura = undefined
+figura :: a -> Dibujo a
+figura a = Figura a
 
 rotar = undefined
 
@@ -40,44 +69,77 @@ juntar = undefined
 
 encimar = undefined
 
+-- Composición n-veces de una función con sí misma. Componer 0 veces
+-- es la función constante, componer 1 vez es aplicar la función 1 vez, etc.
+-- Componer negativamente es un error!
+comp :: (a -> a) -> Int -> a -> a
+comp f n a
+  | n < 0 = error "Componer negativamente es un error!"
+  | n == 0 = a
+  | otherwise = f (comp f (n - 1) a)
 
 -- Rotaciones de múltiplos de 90.
-r180 = undefined
+r180 :: Dibujo a -> Dibujo a
+r180 a = comp rotar 2 a
 
 r270 :: Dibujo a -> Dibujo a
-r270 = undefined
+r270 a = comp rotar 3 a
 
 -- Pone una figura sobre la otra, ambas ocupan el mismo espacio.
-(.-.) = undefined
+(.-.) :: Dibujo a -> Dibujo a -> Dibujo a
+(.-.) d1 d2 = Apilar 1.0 1.0 d1 d2
 
 -- Pone una figura al lado de la otra, ambas ocupan el mismo espacio.
-(///) = undefined
+(///) :: Dibujo a -> Dibujo a -> Dibujo a
+(///) d1 d2 = Juntar 1.0 1.0 d1 d2
 
 -- Superpone una figura con otra.
-(^^^) = undefined
+(^^^) :: Dibujo a -> Dibujo a -> Dibujo a
+(^^^) d1 d2 = Encimar d1 d2
 
 -- Dadas cuatro figuras las ubica en los cuatro cuadrantes.
-cuarteto = undefined
+cuarteto :: Dibujo a -> Dibujo a -> Dibujo a -> Dibujo a -> Dibujo a
+cuarteto d1 d2 d3 d4 = (d1 /// d2) .-. (d3 /// d4)
 
 -- Una figura repetida con las cuatro rotaciones, superpuestas.
-encimar4 = undefined
+encimar4 :: Dibujo a -> Dibujo a
+encimar4 d = (d ^^^ (rotar d)) ^^^ ((r180 d) ^^^ (r270 d))
 
 -- Cuadrado con la misma figura rotada i * 90, para i ∈ {0, ..., 3}.
 -- No confundir con encimar4!
-ciclar = undefined
+ciclar :: Dibujo a -> Dibujo a
+ciclar d = cuarteto d (rotar d) (r180 d) (r270 d)
 
--- Estructura general para la semántica (a no asustarse). Ayuda: 
+-- Estructura general para la semántica (a no asustarse). Ayuda:
 -- pensar en foldr y las definiciones de Floatro a la lógica
-foldDib :: (a -> b) -> (b -> b) -> (b -> b) -> (b -> b) ->
-       (Float -> Float -> b -> b -> b) -> 
-       (Float -> Float -> b -> b -> b) -> 
-       (b -> b -> b) ->
-       Dibujo a -> b
+foldDib ::
+  (a -> b) ->
+  (b -> b) ->
+  (b -> b) ->
+  (b -> b) ->
+  (Float -> Float -> b -> b -> b) ->
+  (Float -> Float -> b -> b -> b) ->
+  (b -> b -> b) ->
+  Dibujo a ->
+  b
 foldDib = undefined
 
 -- Demostrar que `mapDib figura = id`
 mapDib :: (a -> Dibujo b) -> Dibujo a -> Dibujo b
-mapDib = undefined
+mapDib f (Figura a) = f a
+mapDib f (Rotar d) = Rotar (mapDib f d)
+mapDib f (Espejar d) = Espejar (mapDib f d)
+mapDib f (Rot45 d) = Rot45 (mapDib f d)
+mapDib f (Apilar x y d1 d2) = Apilar x y (mapDib f d1) (mapDib f d2)
+mapDib f (Juntar x y d1 d2) = Juntar x y (mapDib f d1) (mapDib f d2)
+mapDib f (Encimar d1 d2) = Encimar (mapDib f d1) (mapDib f d2)
 
 -- Junta todas las figuras básicas de un dibujo.
-figuras = undefined
+figuras :: Dibujo a -> [a]
+figuras (Figura a) = [a]
+figuras (Rotar d) = figuras d
+figuras (Espejar d) = figuras d
+figuras (Rot45 d) = figuras d
+figuras (Apilar x y d1 d2) = figuras d1 ++ figuras d2
+figuras (Juntar x y d1 d2) = figuras d1 ++ figuras d2
+figuras (Encimar d1 d2) = figuras d1 ++ figuras d2
