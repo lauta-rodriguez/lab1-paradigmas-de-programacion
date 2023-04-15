e63b2a59365e65afbeb23e98cc32f8cdc6817827
lara.kurtz <lara.kurtz@mi.unc.edu.ar>
2023-04-15 15:17:14 -0300

    implemented tests for constructor functions. 'figura' and 'rotar' fail

--- src/Tests/TestDibujo.hs
+++ src/Tests/TestDibujo.hs
@@ -21,57 +21,108 @@ import Dibujo
   )
 import Test.HUnit (Test (..), assertEqual)
 
-testfigura :: Test                           
-testfigura = TestCase $ assertEqual "figura" (figura 5) (figura 5)
+testfigura :: Test
+testfigura = 
+  TestCase $ assertEqual 
+    "figura" 
+      (show $ figura 5)
+      "Figura 5"
 
 testrotar :: Test
-testrotar = TestCase $ assertEqual "rotar" (rotar (figura 5)) (rotar (figura 5))
+testrotar =
+  TestCase $ assertEqual
+    "rotar"
+      (show $ rotar (figura 5))
+      "Rotar (Figura 5)"
 
 testespejar :: Test
-testespejar = TestCase $ assertEqual "espejar" (espejar (figura 5)) (espejar (figura 5))
+testespejar =
+  TestCase $ assertEqual
+    "espejar"
+      (show $ espejar (figura 5))
+      "Espejar (Figura 5)"
 
 testrot45 :: Test
-testrot45 = TestCase $ assertEqual "rot45" (rot45 (figura 5)) (rot45 (figura 5))
+testrot45 =
+  TestCase $ assertEqual
+    "rot45"
+      (show $ rot45 (figura 5))
+      "Rot45 (Figura 5)"
 
 testapilar :: Test
-testapilar = TestCase $ assertEqual "apilar" (apilar 2.0 3.0 (figura 1) (figura 2)) (apilar 2.0 3.0 (figura 1) (figura 2))
+testapilar =
+  TestCase $ assertEqual
+    "apilar"
+      (show $ apilar 2.0 3.0 (figura 1) (figura 2))
+      "Apilar 2.0 3.0 (Figura 1) (Figura 2)"
 
 testjuntar :: Test
-testjuntar = TestCase $ assertEqual "juntar" (juntar 2.0 3.0 (figura 1) (figura 2)) (juntar 2.0 3.0 (figura 1) (figura 2))
+testjuntar =
+  TestCase $ assertEqual
+    "juntar"
+      (show $ juntar 2.0 3.0 (figura 1) (figura 2))
+      "Juntar 2.0 3.0 (Figura 1) (Figura 2)"
 
 testencimar :: Test
-testencimar = TestCase $ assertEqual "encimar" (encimar (figura 1) (figura 2)) (encimar (figura 1) (figura 2))
+testencimar =
+  TestCase $ assertEqual
+    "encimar"
+      (show $ encimar (figura 1) (figura 2))
+      "Encimar (Figura 1) (Figura 2)"
 
 testR180 :: Test
-testR180 = TestCase $ assertEqual "r180" (rotar (rotar (figura 5))) (r180 (figura 5))
+testR180 =
+  TestCase $ assertEqual
+    "r180"
+      (show $ r180 (figura 5))
+      "Rotar (Rotar (Figura 5))"
 
 testR270 :: Test
-testR270 = TestCase $ assertEqual "r270" (rotar (rotar (rotar (figura 5)))) (r270 (figura 5))
+testR270 =
+  TestCase $ assertEqual
+    "r270"
+      (show $ r270 (figura 5))
+      "Rotar (Rotar (Rotar (Figura 5)))"
 
 testapilarOp :: Test
-testapilarOp = TestCase $ assertEqual "(.-.)" (apilar 1.0 1.0 (figura 1) (figura 2)) ((figura 1) .-. (figura 2))
+testapilarOp =
+  TestCase $ assertEqual
+    "(.-.)"
+      (show $ figura 1 .-. figura 2)
+      "Apilar 1.0 1.0 (Figura 1) (Figura 2)"
 
 testjuntarOp :: Test
-testjuntarOp = TestCase $ assertEqual "(///)" (juntar 1.0 1.0 (figura 1) (figura 2)) ((figura 1) /// (figura 2))
+testjuntarOp =
+  TestCase $ assertEqual
+    "(///)"
+      (show $ figura 1 /// figura 2)
+      "Juntar 1.0 1.0 (Figura 1) (Figura 2)"
 
 testencimarOp :: Test
-testencimarOp = TestCase $ assertEqual "(^^^)" (encimar (figura 1) (figura 2)) ((figura 1) ^^^ (figura 2))
+testencimarOp =
+  TestCase $ assertEqual
+    "(^^^)"
+      (show $ figura 1 ^^^ figura 2)
+      "Encimar (Figura 1) (Figura 2)"
 
 testCuarteto :: Test
 testCuarteto =
-  TestCase $
-    assertEqual
-      "cuarteto"
-      (apilar 1.0 1.0 ((figura 1) /// (figura 2)) ((figura 3) /// (figura 4)))
-      (cuarteto (figura 1) (figura 2) (figura 3) (figura 4))
+  TestCase $ assertEqual
+    "cuarteto"
+      (show $ 
+        cuarteto (figura 1) (figura 2) (figura 3) (figura 4))
+      "Apilar 1.0 1.0 (Figura 1 /// Figura 2) (Figura 3 /// Figura 4)"
 
 testencimar4 :: Test
 testencimar4 =
-  TestCase $
-    assertEqual
-      "encimar4"
-      (encimar4 (figura 1))
-      (encimar (encimar (encimar (figura 1) (rotar (figura 1))) (rotar (rotar (figura 1)))) (rotar (rotar (rotar (figura 1)))))
+  TestCase $ assertEqual
+    "encimar4"
+      (show $ encimar4 (figura 1))
+      "Encimar (Encimar (Encimar (Figura 1) (Rotar Figura 1)) (Rotar (Rotar Figura 1))) (Rotar (Rotar (Rotar Figura 1)))"
 
 testescalar :: Test
-testescalar = TestCase $ assertEqual "escalar" (escalar 2.0 2.0 (figura 1)) (escalar 2.0 2.0 (figura 1))
+testescalar =
+  TestCase $ assertEqual
+    "escalar"
+      (show $ escalar 2.0 2.0 (figura 1))
+      "Escalar 2.0 2.0 (Figura 1)"
