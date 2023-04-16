3e236dc01b403b778870eccdf70a93a09e6baffe
lara.kurtz <lara.kurtz@mi.unc.edu.ar>
2023-04-16 13:04:21 -0300

    implemented new test cases and fixed formatting

--- src/Tests/TestPred.hs
+++ src/Tests/TestPred.hs
@@ -21,17 +21,51 @@ import Dibujo
 import Pred (Pred, allDib, andP, anyDib, cambiar, orP)
 import Test.HUnit (Test (..), assertEqual)
 
-testalldib :: Test
-testalldib = TestCase $ assertEqual "allDib" (allDib (== 1) (figura 1)) True
+testalldibTrue :: Test
+testalldibTrue =
+  TestCase $ assertEqual
+    "allDib"
+      (allDib (== 1) (apilar 1 1 (figura 1) (figura 1)))
+      True
 
-testanydib :: Test
-testanydib = TestCase $ assertEqual "anyDib" (anyDib (== 1) (figura 1)) True
+testalldibFalse :: Test
+testalldibFalse =
+  TestCase $ assertEqual
+    "allDib"
+      (allDib (== 1) (apilar 1 1 (figura 1) (figura 2)))
+      False
+
+testanydibTrue :: Test
+testanydibTrue =
+  TestCase $ assertEqual
+    "anyDib"
+      (anyDib (== 1) (apilar 1 1 (figura 1) (figura 2)))
+      True
+
+testanydibFalse :: Test
+testanydibFalse =
+  TestCase $ assertEqual
+    "anyDib"
+      (anyDib (== 1) (apilar 1 1 (figura 2) (figura 2)))
+      False
 
 testcambiar :: Test
-testcambiar = TestCase $ assertEqual "cambiar" (cambiar (== 1) (const (figura 2)) (figura 1)) (figura 2)
+testcambiar =
+  TestCase $ assertEqual
+    "cambiar"
+      (cambiar (== 1) (const (figura 2)) (figura 1))
+      (figura 2)
 
 testandp :: Test
-testandp = TestCase $ assertEqual "andP" (andP (== 1) (== 2) 1) False
+testandp =
+  TestCase $ assertEqual
+    "andP"
+      (andP (== 1) (== 2) 1)
+      False
 
 testorp :: Test
-testorp = TestCase $ assertEqual "orP" (orP (== 1) (== 2) 1) True
+testorp =
+  TestCase $ assertEqual
+    "orP"
+      (orP (== 1) (== 2) 1)
+      True
